3d1601186d7a692ed69b2014c801365d62d5cda2
SUN-0207 <cuthanhson27@gmail.com>
2022-12-06 21:11:37 +0700

    sua duong dan trong login va register

--- views/main/login/index.php
+++ views/main/login/index.php
@@ -2,60 +2,61 @@
 <html lang="en">
 
 <head>
-    <title>SQT2-GLOBAL - Login</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="/SQT2-GLOBAL/public/uploads/favicon.png" />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/vendor/bootstrap/css/bootstrap.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/fonts/iconic/css/material-design-iconic-font.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/vendor/animate/animate.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/vendor/css-hamburgers/hamburgers.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/vendor/animsition/css/animsition.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/vendor/select2/select2.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/vendor/daterangepicker/daterangepicker.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/css/util.css">
-    <link rel="stylesheet" type="text/css" href="/SQT2-GLOBAL/public/css/main.css">
-    <!--===============================================================================================-->
+  <title>SQT2 GLOBAL - Login</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--===============================================================================================-->
+  <link rel="icon" type="image/png" href="/SQT2 GLOBAL/public/uploads/favicon.png" />
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css"
+    href="/SQT2 GLOBAL/public/fonts/iconic/css/material-design-iconic-font.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/vendor/animate/animate.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/vendor/animsition/css/animsition.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/vendor/select2/select2.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/vendor/daterangepicker/daterangepicker.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/css/util.css">
+  <link rel="stylesheet" type="text/css" href="/SQT2 GLOBAL/public/css/main.css">
+  <!--===============================================================================================-->
 
 </head>
 
 
 <body>
 
-    <div class="limiter">
-        <div class="container-login100" style="background-image: url('/SQT2-GLOBAL/public/img/background.jpg');">
-            <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+  <div class="limiter">
+    <div class="container-login100" style="background-image: url('/SQT2 GLOBAL/public/img/background.jpg');">
+      <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 
-                <form method="POST" class="login100-form validate-form">
+        <form method="POST" class="login100-form validate-form" data-aos="fade-up">
 
-                    <span class="login100-form-title p-b-49">
-                        Sign In
-                    </span>
+          <span class="login100-form-title p-b-49">
+            Sign In
+          </span>
 
-                    <div class="wrap-input100 validate-input m-b-23" data-validate="Email is required">
-                        <span class="label-input100">Email</span>
-                        <input class="input100" type="text" name="email" placeholder="Type your email">
-                        <span class="focus-input100" data-symbol="&#xf206;"></span>
-                    </div>
+          <div class="wrap-input100 validate-input m-b-23" data-validate="Email is required">
+            <span class="label-input100">Email</span>
+            <input class="input100" type="text" name="email" placeholder="Type your email">
+            <span class="focus-input100" data-symbol="&#xf206;"></span>
+          </div>
 
-                    <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="password" placeholder="Type your password">
-                        <span class="focus-input100" data-symbol="&#xf190;"></span>
-                    </div>
+          <div class="wrap-input100 validate-input" data-validate="Password is required">
+            <span class="label-input100">Password</span>
+            <input class="input100" type="password" name="password" placeholder="Type your password">
+            <span class="focus-input100" data-symbol="&#xf190;"></span>
+          </div>
 
-                    <?php
+          <?php
 
                     if (isset($err)) {
                         echo '<p class="login-box-msg" style="color: red">' . $err . '</p>';
@@ -63,64 +64,64 @@
                     }
                     ?>
 
-                    <div class="text-right -t-8 p-b-31">
-                        <a href="#" style="color:grey">
-                            Forgot password?
-                        </a>
-                    </div>
+          <div class="text-right -t-8 p-b-31">
+            <a href="#" style="color:grey">
+              Forgot password?
+            </a>
+          </div>
 
-                    <div class="container-login100-form-btn">
-                        <div class="wrap-login100-form-btn">
-                            <div class="login100-form-bgbtn"></div>
-                            <button class="login100-form-btn" name="submit-btn">
-                                <strong>sign in</strong>
-                            </button>
-                        </div>
-                    </div>
-
-
-
-                    <div class="flex-col-c p-t-70">
-                        <span class="txt1 p-b-0">
-                            <a href="index.php?page=main&controller=register&action=index" style="color: grey;">Sign Up</a>
-                        </span>
-                    </div>
-
-
-                    <div class="flex-col-c p-t-20">
-                        <span class="txt1 p-b-0">
-                            <a href="index.php?page=main&controller=layouts&action=index" style="color: grey;">Return Home</a>
-                        </span>
-                    </div>
-
+          <div class="container-login100-form-btn">
+            <div class="wrap-login100-form-btn">
+              <div class="login100-form-bgbtn"></div>
+              <button class="login100-form-btn" name="submit-btn">
+                <strong>sign in</strong>
+              </button>
             </div>
+          </div>
 
 
 
-            </form>
-        </div>
+          <div class="flex-col-c p-t-70">
+            <span class="txt1 p-b-0">
+              <a href="index.php?page=main&controller=register&action=index" style="color: grey;">Sign Up</a>
+            </span>
+          </div>
+
+
+          <div class="flex-col-c p-t-20">
+            <span class="txt1 p-b-0">
+              <a href="index.php?page=main&controller=layouts&action=index" style="color: grey;">Return Home</a>
+            </span>
+          </div>
+
+      </div>
+
+
+
+      </form>
     </div>
-    </div>
+  </div>
+  </div>
 
 
-    <div id="dropDownSelect1"></div>
+  <div id="dropDownSelect1"></div>
 
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/vendor/jquery/jquery-3.2.1.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/vendor/animsition/js/animsition.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/vendor/bootstrap/js/popper.js"></script>
-    <script src="/SQT2-GLOBAL/public/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/vendor/select2/select2.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/vendor/daterangepicker/moment.min.js"></script>
-    <script src="/SQT2-GLOBAL/public/vendor/daterangepicker/daterangepicker.js"></script>
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/vendor/countdowntime/countdowntime.js"></script>
-    <!--===============================================================================================-->
-    <script src="/SQT2-GLOBAL/public/js/main.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/vendor/jquery/jquery-3.2.1.min.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/vendor/animsition/js/animsition.min.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/vendor/bootstrap/js/popper.js"></script>
+  <script src="/SQT2 GLOBAL/public/vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/vendor/select2/select2.min.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/vendor/daterangepicker/moment.min.js"></script>
+  <script src="/SQT2 GLOBAL/public/vendor/daterangepicker/daterangepicker.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/vendor/countdowntime/countdowntime.js"></script>
+  <!--===============================================================================================-->
+  <script src="/SQT2 GLOBAL/public/js/main.js"></script>
 
 </body>
 
